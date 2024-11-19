5e3986026447c3bca5082da829961c9c8a5d10e5
Thiago <nescauepic@gmail.com>
2024-11-19 12:23:07 -0300

    presente de aniversario

---
+++ presente-nequinha/nascimento-digo/config.js
@@ -0,0 +1,55 @@
+
+var config = {
+ 
+    texts: [
+       "Você é maravilhosa", 
+        "cada momento ao seu lado é especial",
+        "que continue sendo essa mulher perfeita que vc é" ,
+        "tenho muito orgulho de você",
+        "tudo com você é mais legal",
+        "estar com vc faz o mundo ter cor",
+        "você é" ,
+        "perfeita" ,
+        "gostosa" ,
+        "maravilhosa",
+        "sensacional",
+        "deslumbrante" ,
+        "magnifica" ,
+        "já falei gostosa?" ,
+        "UM ABSURDO", 
+        "Feliz aníversario", 
+    ],
+   
+    imgs: {
+        "Você é maravilhosa": "./imgs/sorriso.jpeg:",
+        "cada momento ao seu lado é especial": "./imgs/solzin.jpeg",
+        "que continue sendo essa mulher perfeita que vc é": "./imgs/bjbeijin.jpeg",
+        "tenho muito orgulho de você": "./imgs/espelho.jpeg",
+        "tudo com você é mais legal": "./imgs/efeito.jpeg",
+        "estar com vc faz o mundo ter cor": "./imgs/bj.jpeg",
+        "você é": "./imgs/sinais.jpeg",
+        "perfeita": "./imgs/sefeito.jpeg",
+        "gostosa": "./imgs/mateus.jpeg",
+        "maravilhosa": "./imgs/black.jpeg",
+        "sensacional": "./imgs/gif.mp4",
+        "deslumbrante": "./imgs/sol.jpeg",
+        "magnifica": "./imgs/.jpeg",
+        "já falei gostosa?": "./imgs/nothing.jpg",
+        "UM ABSURDO": "./imgs/nothing.jpg",
+        "Feliz aníversario": "./imgs/last.jpeg"
+    },
+
+    desc: {
+        turn_on: "pronta?",
+        play: "uma musiquinha pra melhorar!",
+        bannar_coming: "e desejo pra você",
+        balloons_flying: "não seria uma festa sem balões",
+        cake_fadein: "mas e o bolo？",
+        light_candle: "posso acender as velas？",
+        wish_message: "o discurso não pode faltar",
+        story: "uma mensagem pra vc morzinho"
+    },
+
+
+    loveText: 'EU TE AMO'
+};
